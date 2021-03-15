12b7ecccdb4395898d57f16f1c7f067e2306bfc5
Diana Henao <2777@holbertonschool.com>
2021-03-15 20:33:27 +0000

    :rocket: Initial commit

---
+++ _print.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+
+/**
+* _printf - C - printf
+* @format: Input string with specifiers
+*
+* Description: A function that produces output
+* according to a given format.
+* Return: Number of characters printed (excluding
+* the null byte used to end output to strings).
+*/
+int _printf(const char *format, ...)
+{
+	int i, j;
+	int n_words, n_func;
+	va_list arg;
+
+	n_words = 0;
+	n_func = 6;
+	specifier select_spec[] = {
+		{"c", op_ch}, {"%", op_pr},
+		{"s", op_st}, {"d", op_nu},
+		{"i", op_in}, {"f", op_fl},
+		{NULL, NULL}
+		};
+
+	if (format == NULL)
+		return (0);
+
+	va_start(arg, format);
+
+	for (i = 0; format[i]; i++)
+	{
+		if (format[i] != '%')
+			n_words = n_words + _putchar(format[i]);
+
+		else
+		{
+			for (j = 0; j < n_func; j++)
+			{
+				if (format[i + 1] == select_spec[j].ch[0])
+				{
+					n_words = select_spec[j].f(arg) + n_words;
+					break;
+				}
+			}
+			i++;
+		}
+	}
+
+	va_end(arg);
+	return (n_words);
+}
