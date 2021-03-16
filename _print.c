335a837b63ad20e1114f6ce20502234afd372517
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-16 21:40:34 +0000

    :monocle_face: possible solution

--- _print.c
+++ _print.c
@@ -1,5 +1,4 @@
 #include "holberton.h"
-
 /**
 * _printf - C - function that give number of chars
 * @format: Input string with specifiers
@@ -12,35 +11,35 @@
 int _printf(const char *format, ...)
 {
 	int i, j;
-	int n_words, n_func;
+	int n_words = 0, n_func = 7;
 	va_list arg;
-
 	specifier select_spec[] = {
 		{"c", op_ch}, {"s", op_st}, {"d", op_nu}, {"i", op_nu},
 		{"f", op_fl}, {"b", op_bi}, {"R", _rot13}, {NULL, NULL}};
-
-	n_words = 0;
-	n_func = 7;
 	if (format == NULL)
 		return (-1);
 	va_start(arg, format);
-
 	for (i = 0; format[i] != '\0' && format != NULL; i++)
 	{
 		if (format[i] != '%')
 			n_words = n_words + _putchar(format[i]);
-
 		else
 		{
 			for (j = 0; j < n_func; j++)
 			{
 				if (format[i + 1] == select_spec[j].ch[0])
 				{
-					n_words = select_spec[j].f(arg) + n_words;
+					n_words += select_spec[j].f(arg);
+					break;
+				}
+				else if (format[i + 1] != select_spec[j].ch[0] && format[i + 1] != '\0')
+				{
+					n_words = n_words + _putchar(format[i]);
+					_putchar(format[i + 1]);
+					n_words++;
 					break;
 				}
 			}
-
 			if (format[i + 1] == '%')
 				n_words = n_words + _putchar('%');
 			else if (i == 0 && format[i + 1] == '\0')
