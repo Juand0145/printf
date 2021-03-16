79f03b14a7e75b0a57ec5086b8080ae489c47208
jyuly12 <yuly.gonzalez2021@gmail.com>
2021-03-16 16:32:55 +0000

    :adhesive_bandage: a little fix

--- other_functions.c
+++ other_functions.c
@@ -12,29 +12,31 @@ char letter[] = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
 char rotcode[] = "NnOoPpQqRrSsTtUuVvWwXxYyZzAaBbCcDdEeFfGgHhIiJjKkLlMm";
 char *str = va_arg(agr, char*);
 i = j = count = 0;
-	if (str != NULL)
+
+if (!str)
+{
+	_printf("(null)");
+	return (6);
+}
+	for (; str[i]; i++)
 	{
-		for (; str[i]; i++)
+		h = 0;
+		for (j = 0; letter[j]; j++)
 		{
-			h = 0;
-			for (j = 0; letter[j]; j++)
+			if (str[i] == letter[j])
 			{
-				if (str[i] == letter[j])
-				{
-					_putchar(rotcode[j]);
-					h = 1;
-					count++;
-				}
-			}
-			if (!h)
-			{
-				_putchar(str[i]);
+				_putchar(rotcode[j]);
+				h = 1;
 				count++;
 			}
 		}
+		if (!h)
+		{
+			_putchar(str[i]);
+			count++;
+		}
+
 	}
-	else
-		_printf("(null)");
 
 	return (count);
 }
